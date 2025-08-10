2a460aac17477976c20dbb037197bed0eaf67112
Iossif Benbassat <iossif123gg@gmail.com>
2025-08-10 08:40:21 +0300

    increase side panel width

--- client/src/shared/components/SidePanel/SidePanelWrapper.tsx
+++ client/src/shared/components/SidePanel/SidePanelWrapper.tsx
@@ -14,7 +14,7 @@ export default function SidePanelWrapper({ children }: Props) {
         className="fixed inset-0 bg-black/50 z-40"
         onClick={closeSidePanel}
       />
-      <div className="fixed right-0 top-0 h-full w-96 bg-base-100 border-l border-base-300 shadow-xl z-50 overflow-y-auto">
+      <div className="fixed right-0 top-0 h-full w-196 bg-base-100 border-l border-base-300 shadow-xl z-50 overflow-y-auto">
         {children}
       </div>
     </>
