409ef417af08d3d186d31ef619dcef39ac98b10d
Iossif Benbassat <iossif123gg@gmail.com>
2025-08-16 17:16:01 +0300

    minor fix

--- server/src/channels/channel.controller.ts
+++ server/src/channels/channel.controller.ts
@@ -1,15 +1,9 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ChannelService } from './channel.service';
-import { createChannelDto } from './dtos/create-channel.dto';
 
 @Controller('channel')
 export class ChannelController {
   constructor(private readonly channelService: ChannelService) {}
-
-  @Post()
-  create(@Body() createChannelDto: createChannelDto) {
-    return this.channelService.create(createChannelDto);
-  }
 
   @Get('/metadata/:ytChannelId')
   metadata(@Param('ytChannelId') ytChannelId: string) {
