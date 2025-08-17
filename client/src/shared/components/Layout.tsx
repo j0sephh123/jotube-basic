bce00ed5badc0142d901a61528ad481bbade796a
Iossif Benbassat <iossif123gg@gmail.com>
2025-08-17 12:50:09 +0300

    start moving to widgets

--- client/src/shared/components/Layout.tsx
+++ client/src/shared/components/Layout.tsx
@@ -2,15 +2,15 @@ import { Outlet } from "react-router-dom";
 import TheCarousel from "@/features/Screenshot/components/TheCarousel";
 import { useWebSocket } from "@/shared/hooks/useWebSocket";
 import { useGlobalWebSocket } from "@/shared/hooks/useGlobalWebSocket";
-import TheNavbar from "@/shared/components/Navbar";
 import { DialogProvider } from "./dialog/DialogProvider";
 import ThumbnailsProcessing from "@/features/Thumbnail";
-import ScreenshotsSidePanel from "./ScreenshotsSidePanel";
 import { AddChannelToPlaylistModal } from "@/features/Playlist/components/AddChannelToPlaylistModal";
 import ZoomModal from "./ZoomModal";
 import StoryboardProcessing from "@/features/Storyboard/StoryboardProcessing";
 import CreateChannel from "@/features/Channel/CreateChannel";
 import { ToastProvider } from "../../app/providers/ToastProvider";
+import ScreenshotsSidePanel from "@/widgets/ScreenshotsSidePanel";
+import Navbar from "@/widgets/Navbar";
 
 export default function Layout(): JSX.Element {
   useWebSocket();
@@ -18,7 +18,7 @@ export default function Layout(): JSX.Element {
   return (
     <ToastProvider>
       <DialogProvider>
-        <TheNavbar />
+        <Navbar />
         <div className="w-full h-[99vh] overflow-auto">
           <Outlet />
         </div>
