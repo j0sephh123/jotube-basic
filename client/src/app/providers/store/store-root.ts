fd5230a374da53dd3b494f40bc75fa024906fbd7
Iossif Benbassat <iossif123gg@gmail.com>
2025-08-27 19:37:11 +0300

    minor fix

--- client/src/app/providers/store/store-root.ts
+++ client/src/app/providers/store/store-root.ts
@@ -1,4 +1,3 @@
-// client/src/app/providers/store/store-root.ts
 import { create } from "zustand";
 import { createZoomSlice } from "./zoom-slice";
 import { createVideoModalSlice } from "./video-modal-slice";
@@ -6,53 +5,66 @@ import { createGalleryModalSlice } from "./gallery-modal-slice";
 import type { Store as StoreType } from "./store-types";
 import { createCarouselScreenshotsSlice } from "./screenshots-for-carousel-slice";
 import { createThumbnailsSlice } from "./thumbnails-slice";
+import { SortOrder } from "@shared/api";
+import { ViewType } from "@features/Dashboard";
 
-export const useStore = create<StoreType>()(
-  (set, _get) =>
-    ({
-      ...createZoomSlice(set),
-      ...createVideoModalSlice(set),
-      ...createGalleryModalSlice(set),
-      ...createThumbnailsSlice(set),
-      ...createCarouselScreenshotsSlice(set),
-      storyboardProcessingData: [],
-      setStoryboardProcessingData: () => {},
-      clearStoryboardProcessingData: () => {},
-      requestBody: {
-        sortOrder: "DESC",
-        page: 1,
-        min: 0,
-        max: null,
-        defaultMin: 0,
-        defaultMax: null,
-        viewType: "saved",
-      },
-      setRequestBody: () => {},
-      rangePickers: {},
-      setRangePicker: () => {},
-      updateRangePickerValues: () => {},
-      getRangePicker: () => undefined,
-      videosRequestBody: {
-        sortOrder: "DESC",
-        page: 1,
-        minScreenshots: 0,
-        maxScreenshots: null,
-      },
-      setVideosRequestBody: () => {},
-      videosRangePickers: {},
-      setVideosRangePicker: () => {},
-      updateVideosRangePickerValues: () => {},
-      getVideosRangePicker: () => undefined,
-      playlists: [],
-      setPlaylists: () => {},
-      addPlaylist: () => {},
-      removePlaylist: () => {},
-      isModalOpen: false,
-      ytChannelId: "",
-      showCreateForm: false,
-      openPlaylistModal: () => {},
-      closePlaylistModal: () => {},
-      setYtChannelId: () => {},
-      setShowCreateForm: () => {},
-    } as unknown as StoreType)
-);
+export const useStore = create<StoreType>()((set, _get) => ({
+  ...createZoomSlice(set),
+  ...createVideoModalSlice(set),
+  ...createGalleryModalSlice(set),
+  ...createThumbnailsSlice(set),
+  ...createCarouselScreenshotsSlice(set),
+  storyboardProcessingData: [],
+  setStoryboardProcessingData: () => {},
+  clearStoryboardProcessingData: () => {},
+  requestBody: {
+    sortOrder: SortOrder.Desc,
+    page: 1,
+    min: 0,
+    max: null,
+    defaultMin: 0,
+    defaultMax: null,
+    viewType: ViewType.SAVED,
+  },
+  setRequestBody: () => {},
+  rangePickers: {
+    processed: {
+      values: [],
+      min: 0,
+      max: 0,
+      stepSize: 0,
+    },
+    defaults: {
+      values: [],
+      min: 0,
+      max: 0,
+      stepSize: 0,
+    },
+  },
+  setRangePicker: () => {},
+  updateRangePickerValues: () => {},
+  getRangePicker: () => undefined,
+  videosRequestBody: {
+    sortOrder: SortOrder.Desc,
+    page: 1,
+    minScreenshots: 0,
+    maxScreenshots: null,
+  },
+  setVideosRequestBody: () => {},
+  videosRangePickers: {},
+  setVideosRangePicker: () => {},
+  updateVideosRangePickerValues: () => {},
+  getVideosRangePicker: () => undefined,
+  playlists: [],
+  setPlaylists: () => {},
+  addPlaylist: () => {},
+  removePlaylist: () => {},
+  isModalOpen: false,
+  ytChannelId: "",
+  showCreateForm: false,
+  openPlaylistModal: () => {},
+  closePlaylistModal: () => {},
+  setYtChannelId: () => {},
+  setShowCreateForm: () => {},
+  resetFormState: () => {},
+}));
