ac03334826889a5a7b90f32f83e9802bc06190f7
Iossif Benbassat <iossif123gg@gmail.com>
2025-08-27 16:43:03 +0300

    remove unused method

--- server/src/screenshots/api/screenshots-api.module.ts
+++ server/src/screenshots/api/screenshots-api.module.ts
@@ -4,11 +4,10 @@ import { ScreenshotsApiService } from './screenshots-api.service';
 import { DatabaseModule } from 'src/core/database/database.module';
 import { FileModule } from 'src/file/file.module';
 import { ScreenshotsManagerModule } from 'src/screenshots/manager/screenshots-manager.module';
-import { ScreenshotsResolver } from '../screenshots.resolver';
 
 @Module({
   imports: [ScreenshotsManagerModule, FileModule, DatabaseModule],
   controllers: [ScreenshotsApiController],
-  providers: [ScreenshotsApiService, ScreenshotsResolver],
+  providers: [ScreenshotsApiService],
 })
 export class ScreenshotsApiModule {}
