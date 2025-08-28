578c63f312c11d657a38fe7123cb2634d6e1f6b4
Iossif Benbassat <iossif123gg@gmail.com>
2025-08-28 15:55:56 +0300

    remove more playlist confusing fnctions

--- client/src/features/Playlist/hooks/index.ts
+++ client/src/features/Playlist/hooks/index.ts
@@ -4,7 +4,6 @@ export { useGetPlaylist } from "@features/Playlist/hooks/useGetPlaylist";
 export { useUpdatePlaylist } from "@features/Playlist/hooks/useUpdatePlaylist";
 export { useDeletePlaylist } from "@features/Playlist/hooks/useDeletePlaylist";
 export { useUpdateChannelPlaylist } from "@features/Playlist/hooks/useUpdateChannelPlaylist";
-export { usePlaylist } from "@features/Playlist/hooks/usePlaylist";
 export { useRefetchPlaylist } from "@features/Playlist/hooks/useGetPlaylist";
 export { useGetPlaylistUploads } from "@features/Playlist/hooks/useGetPlaylistUploads";
 export { useRefetchPlaylistUploads } from "@features/Playlist/hooks/useGetPlaylistUploads";
