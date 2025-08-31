cfc7b7fe16788a359d583688eeef9d8e3c05a53d
Iossif Benbassat <iossif123gg@gmail.com>
2025-08-31 10:21:22 +0300

    use channel title in manual thumbnail picker

--- client/src/features/Thumbnails/ui/Header.tsx
+++ client/src/features/Thumbnails/ui/Header.tsx
@@ -1,25 +1,28 @@
 import { SmoothProgressBar, useThumbnailsCount } from "@features/Thumbnails";
 import { useProcessingState } from "@shared/store";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  const { items: thumbnailsProcessingData, currentIndex } =
-    useProcessingState();
+  const { items, currentIndex } = useProcessingState();
   const thumbnailsCount = useThumbnailsCount();
 
-  const current = currentIndex || 0;
-  const total = thumbnailsCount || 0;
+  const ytChannelId = items?.[0]?.ytChannelId ?? "";
+  const ytVideoId = items?.[0]?.ytVideoId ?? "";
+  const channelTitle = items?.[0]?.channelTitle ?? "";
+
+  const current = currentIndex ?? 0;
+  const total = thumbnailsCount ?? 0;
 
   return (
     <div className="flex flex-col gap-4 p-4 bg-gray-800">
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-2">
-          <h2 className="text-xl font-bold">Processing Thumbnails</h2>
-          <div className="ml-4 text-gray-300 text-sm">
-            Channel: {thumbnailsProcessingData[0]?.ytChannelId ?? ""}
-          </div>
-          <div className="text-gray-300 text-sm">
-            Video: {thumbnailsProcessingData[0]?.ytVideoId ?? ""}
-          </div>
+          <Link to={`/channels/${ytChannelId}/saved`}>
+            <div className="ml-4 text-gray-300 text-sm">
+              Channel: {channelTitle ?? ""}
+            </div>
+          </Link>
+          <div className="text-gray-300 text-sm">Video: {ytVideoId ?? ""}</div>
           <div className="text-gray-300 text-sm">
             {currentIndex + 1} / {thumbnailsCount}
           </div>
