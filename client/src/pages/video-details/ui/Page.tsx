dcdff83e05d6f683943ff0d35b48bb39f957d27a
Iossif Benbassat <iossif123gg@gmail.com>
2025-09-02 21:10:44 +0300

    open gallery for video

--- client/src/pages/video-details/ui/Page.tsx
+++ client/src/pages/video-details/ui/Page.tsx
@@ -11,6 +11,7 @@ import { VideoFiles } from "./VideoFiles";
 import { VideoDetailsWrapper } from "./VideoDetailsWrapper";
 import { setProcessingData } from "@shared/store";
 import { useScreenshotsForCarousel } from "@features/Screenshot";
+import { setGalleryModal } from "@features/Gallery";
 
 export function VideoDetailsPage() {
   const ytChannelId = useTypedParams("ytChannelId");
@@ -35,6 +36,13 @@ export function VideoDetailsPage() {
   };
 
   const handleViewScreenshots = useScreenshotsForCarousel(ytVideoId);
+
+  const handleGalleryClick = () => {
+    setGalleryModal({
+      ytVideoId,
+      ytChannelIds: [ytChannelId],
+    });
+  };
 
   if (!data) return null;
 
@@ -69,7 +77,7 @@ export function VideoDetailsPage() {
           <div className="space-y-4">
             <div className="bg-white dark:bg-gray-800 rounded-lg shadow-sm border border-gray-200 dark:border-gray-600 p-4">
               <div className="flex gap-2">
-                {/* <Button onClick={handleViewScreenshots}>Gallery</Button> */}
+                <Button onClick={handleGalleryClick}>Gallery</Button>
                 <Button onClick={() => handleViewScreenshots([ytChannelId])}>Screenshots</Button>
                 <Button onClick={handleViewThumbnails}>Thumbnails</Button>
               </div>
