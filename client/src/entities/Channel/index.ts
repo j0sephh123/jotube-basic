71b432c5d2eb13468e0b57eb7dd1a7d93b2f83dd
Iossif Benbassat <iossif123gg@gmail.com>
2025-09-03 15:43:08 +0300

    minor

--- client/src/entities/Channel/index.ts
+++
@@ -1,8 +0,0 @@
-export { useGetChannel } from "./model/useGetChannel";
-export {
-  useRefetchChannelMetadata,
-  useChannelMetadataQuery,
-} from "./model/useChannelMetadata";
-export { default as useDeleteChannel } from "../../features/Channel/hooks/useDeleteChannel";
-export { useChannelForPlaylist } from "./api/useChannelForPlaylist.gql";
-export { DeleteChannel } from "./ui";
--- client/src/entities/Channel/model/useGetChannel.ts
+++
@@ -1,5 +0,0 @@
-import { useChannelForPlaylist } from "@entities/Channel";
-
-export const useGetChannel = (ytChannelId: string | null) => {
-  return useChannelForPlaylist(ytChannelId || undefined);
-};
