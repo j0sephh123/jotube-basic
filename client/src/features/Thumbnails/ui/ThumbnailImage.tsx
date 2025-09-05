c6f1238dbd844f63d291f8651015c387a93d0bbd
Iossif Benbassat <iossif123gg@gmail.com>
2025-09-05 21:58:57 +0300

    refactor

--- client/src/features/Thumbnails/ui/ThumbnailImage.tsx
+++ client/src/features/Thumbnails/ui/ThumbnailImage.tsx
@@ -1,17 +1,10 @@
 import { memo } from "react";
-import { generateMainThumbnailUrl } from "@features/Thumbnails";
-import { useThumbnailsProcessingState } from "@shared/store";
 
-const ThumbnailImage = memo(() => {
-  const { items: thumbnailsProcessingData, currentIndex } =
-    useThumbnailsProcessingState();
-  const { ytChannelId, ytVideoId } = thumbnailsProcessingData[0] ?? {};
-  const src = generateMainThumbnailUrl(
-    ytChannelId ?? "",
-    ytVideoId ?? "",
-    currentIndex
-  );
+type Props = {
+  src: string;
+};
 
+const ThumbnailImage = memo(({ src }: Props) => {
   return (
     <img
       className="w-full"
--- client/src/widgets/ManualPicker/ManualThumbnailsPicker.tsx
+++ client/src/widgets/ManualPicker/ManualThumbnailsPicker.tsx
@@ -8,20 +8,24 @@ import {
   useHandleContainerWheel,
   useHandleKeyDown,
   useResetSelection,
+  generateMainThumbnailUrl,
 } from "@features/Thumbnails";
 import { setZoom } from "@features/Screenshot";
 import { setSelectedImages, useThumbnailsProcessingState } from "@shared/store";
 import { generateThumbnailUrl } from "@shared/utils";
 import { useRef } from "react";
-import { makeYtChannelId, To } from "@shared/types";
+import { makeYtChannelId, type To } from "@shared/types";
 
 export function ManualThumbnailsPicker() {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const { items: thumbnailsProcessingData } = useThumbnailsProcessingState();
+  const { items: thumbnailsProcessingData, currentIndex } =
+    useThumbnailsProcessingState();
 
   const ytChannelId = thumbnailsProcessingData[0]?.ytChannelId ?? "";
   const ytVideoId = thumbnailsProcessingData[0]?.ytVideoId ?? "";
+
+  const src = generateMainThumbnailUrl(ytChannelId, ytVideoId, currentIndex);
 
   const handleZoom = (index: number): void => {
     const url = generateThumbnailUrl(ytChannelId, ytVideoId, index);
@@ -42,7 +46,7 @@ export function ManualThumbnailsPicker() {
     <>
       <Header channelLink={channelLink} videoLabel={videoLabel} />
       <Container ref={containerRef}>
-        <ThumbnailImage />
+        <ThumbnailImage src={src} />
         <Grid handleZoom={handleZoom} />
       </Container>
       <Footer />
